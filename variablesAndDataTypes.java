092cfde61f60e5712793b9eb53255af737d56488
SHARON2027 <sharonjohntadi28@gmail.com>
2025-02-03 16:24:10 +0530

    Add files via upload

---
+++ variablesAndDataTypes.java
@@ -0,0 +1,16 @@
+public class VariablesAndDataTypes {
+    public static void main(String[] args) {
+        // Declare and initialize different data types
+        int integerVar = 20;
+        double doubleVar = 10.5;
+        char charVar = 'S';
+        String stringVar = "Hello, This is SHARON";
+        boolean boolVar = true;
+        // Display the values of the variables
+        System.out.println("Integer: " + integerVar);
+        System.out.println("Double: " + doubleVar);
+        System.out.println("Character: " + charVar);
+        System.out.println("String: " + stringVar);
+        System.out.println("Boolean: " + boolVar);
+    }
+}
